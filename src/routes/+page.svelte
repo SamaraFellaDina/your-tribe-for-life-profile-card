9a749825528a59bc7270fdca36326816f5dad702
SamaraFellaDina <sammybensink@gmail.com>
2024-09-05 18:19:24 +0200

    stijling ingevoegd

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,69 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+    <picture>
+        <img src="" alt="profiel foto">
+    </picture>
+
+    <section>
+        <h1>
+            A problem had been detected and Windows has
+            been shut down to prevent damage to
+            your computer.
+        </h1>
+
+        <h2>UNMOUTABLE_BOOT_VOLUME</h2>
+
+        <p>
+            We’ve detected an error called: ”SAMMY-BENSINK.exe”, also known as: “Sambal”.
+            If this is the first time you’ve seen this error, restart your computer.
+            If this screen appears again, you should seek help at: https://github.com/.
+            
+            If you want to know about this issue, seek help bij looking through information
+            noted as: “SamaraFellaDina”. 
+        </p>
+
+        <h2>SAMMY-MESSAGE_ERROR_205</h2>
+        <p>"M&M reclames zijn F*cking irritant"</p>
+    </section>
+</main>
+
+<style>
+    @font-face {
+    font-family: "Pixel";
+    src:
+        url("assets/font/Jersey20-Regular.ttf");
+    }
+
+    :root {
+        background-color: blue;
+    }
+
+    main {
+        font-family: "Pixel", sans-serif;
+        color: white;
+        letter-spacing: .04em;
+        width: 70%;
+    }
+
+    h1,
+    h2 {
+        --h1-size: 3em;
+        --h2-size: 2em;
+
+        font-weight: 500;
+    }
+
+    h1 {
+        font-size: var(--h1-size);
+    }
+
+    h2 {
+        font-size: var(--h2-size);
+    }
+
+    p {
+        --p-size: 1.4em;
+        font-size: var(--p-size);
+        line-height: 1.2em;
+        font-weight: 300;
+    }
+</style>
